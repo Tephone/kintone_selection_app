6f02883df53ca448904c39c58ffd69eb5c6011bd
kassar <kassa0918@gmail.com>
2022-06-06 17:12:14 +0900

    一括更新アプリ(500以上のレコード)位リファクタ

--- mass_update.js
+++ mass_update.js
@@ -25,44 +25,49 @@
         )
     }
 
+    async function massUpdate() {
+        const MAX_GET_LIMIT = 500
+        const MAX_UPDATE_LIMIT = 100
+        const updatedAppId = kintone.app.getId()
+        let offset = 0
+        let notWillBeDone = true
+        let recordsAcquired = []
+
+        while (notWillBeDone) {
+            const paramGet = {
+                'app': updatedAppId,
+                'query': `order by $id asc limit ${MAX_GET_LIMIT} offset ${offset}`
+            }
+            const reps = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', paramGet)
+
+            if (reps.records.length < MAX_GET_LIMIT) {
+                notWillBeDone = false
+            }
+            offset += MAX_GET_LIMIT
+            recordsAcquired = recordsAcquired.concat(reps.records)
+        }
+        const recordsByOneHundred = await sliceByNumber(recordsAcquired, MAX_UPDATE_LIMIT)
+        const promises = recordsByOneHundred.map(records => {
+            const paramPut = {
+                'app': updatedAppId,
+                'records': createPutRecords(records)
+            }
+            return kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', paramPut)
+        })
+        await Promise.all(promises)
+    }
+
     kintone.events.on('app.record.index.show', function (event) {
         const massUpdateButton = document.createElement('button')
         massUpdateButton.id = 'mass_update_button'
         massUpdateButton.innerText = '一括更新する'
-        const updatedAppId = kintone.app.getId()
 
         if (document.getElementById('mass_update_button') !== null) {
             return
         }
 
         massUpdateButton.onclick = async function () {
-            let offset = 0
-            let recordsUnacquired = true
-            let recordsacquired = []
-
-            while (recordsUnacquired) {
-                const paramGet = {
-                    'app': updatedAppId,
-                    'query': `order by $id asc limit 500 offset ${offset}`
-                }
-                const reps = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', paramGet)
-
-                if (reps.records.length < 500) {
-                    recordsUnacquired = false
-                }
-                offset += 500
-                recordsacquired = recordsacquired.concat(reps.records)
-            }
-            const recordsByOneHundred = await sliceByNumber(recordsacquired, 100)
-            const promises = recordsByOneHundred.map(records => {
-                const paramPut = {
-                    'app': updatedAppId,
-                    'records': createPutRecords(records)
-                }
-                return kintone.api(kintone.api.url('/k/v1/records', true), 'PUT', paramPut)
-            })
-            await Promise.all(promises)
-
+            await massUpdate()
             location.reload()
         }
         kintone.app.getHeaderMenuSpaceElement().appendChild(massUpdateButton)
